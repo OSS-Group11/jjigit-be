78c8ac9da660b1157a55b185f0a907cbf7900851
HyerimH <134906042+HyerimH@users.noreply.github.com>
2025-11-16 22:32:20 +0900

    Add files via upload

---
+++ src/main/java/com/jigit/backend/JjigitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jigit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjigitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjigitBackendApplication.class, args);
+	}
+
+}
